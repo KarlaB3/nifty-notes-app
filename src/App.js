0a2e805bbe7a2d6fdf7b735a18be11610f512114
KarlaB3 <k.bucoy@gmail.com>
2023-05-18 09:55:26 +1000

    feat: set up routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<h1>to do</h1>} />
+        <Route path="/notes" element={<h1>to do</h1>} />
+        <Route path="/notes/:noteID" element={<h1>to do</h1>} />
+        <Route path="/notes/:noteID/edit" element={<h1>to do</h1>} />
+        <Route path="/searchByWord/:word" element={<h1>to do</h1>} />
+        <Route path="/sort/duedate" element={<h1>to do</h1>} />
+        <Route path="/sort/createddate" element={<h1>to do</h1>} />
+        <Route path="/filter/overdue" element={<h1>to do</h1>} />
+        <Route path="/filter/done" element={<h1>to do</h1>} />
+      </Routes>
 
     </div>
   );
