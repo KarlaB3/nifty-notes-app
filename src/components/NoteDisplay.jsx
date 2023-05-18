f58bc19f296e2422ea76b45fcb9c0dee84961577
KarlaB3 <k.bucoy@gmail.com>
2023-05-18 18:34:53 +1000

    feat: fixed notesDisplay

--- src/components/NoteDisplay.jsx
+++ src/components/NoteDisplay.jsx
@@ -1,29 +1,31 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useNoteData } from "../contexts/NotesContext";
 
 export default function NoteDisplay(props) {
 
     const {id} = props;
-    let note = useRef({});
+    const [localNote, setLocalNote] = useState({});
 
     const globalNotesData = useNoteData();
 
     useEffect(() => {
         // On start, find the note in globalNotesData that has an ID match props.id
-        note.current = globalNotesData.filter(globalSpecificNote => {
-        return globalSpecificNote.id === id;
-        })
-    }, [globalNotesData], id)
+
+        setLocalNote(globalNotesData.find(globalSpecificNote => {
+            return globalSpecificNote.id === id;
+        }));
+
+    }, [globalNotesData, id])
 
 
     return (
         <div>
-            <h4>{note.title}</h4>
-            <p>{note.description}</p>
-            <p>{note.isCompleted ? "COMPLETE" : "NOT YET COMPLETE"}</p>
-            <input type="checkbox" disabled="disabled" value={note.isCompleted}/>
-            h5>Due Date: {new Date(note.dueDate).toLocaleString()}</h5>
-            <h5>Created At: {new Date(note.createdAtDate).toLocaleString()}</h5>
+            <h4>{localNote.title}</h4>
+            <p>{localNote.description}</p>
+            <p>{localNote.isCompleted ? "COMPLETE" : "NOT YET COMPLETE"}</p>
+            <input type="checkbox" disabled="disabled" value={localNote.isCompleted}/>
+            <h5>Due Date: {new Date(localNote.dueDate).toLocaleString()}</h5>
+            <h5>Created At: {new Date(localNote.createdAtDate).toLocaleString()}</h5>
         </div>
     )
 }
